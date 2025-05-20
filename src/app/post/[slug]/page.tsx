9186668d6d81dcc1763546933685cec9befb6c7b
fenix <fenix.azul09@protonmail.com>
2025-05-20 10:09:30 -0300

    "Arrumando todas funções dos parametros para gerar paginas estaticas"

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,8 +1,7 @@
-import { getPost } from "@/data/posts/get-post";
 import { getAllPosts } from "@/data/posts/get-all-posts";
 import { countAllPosts } from "@/data/posts/count-all-posts";
-import { notFound } from "next/navigation";
-import { Post } from "@/containers/Post";
+import { Post as PostComponent } from "@/containers/Post";
+import { PostData } from "@/domain/posts/post";
 
 interface PostPageProps {
     params: {
@@ -11,29 +10,23 @@ interface PostPageProps {
 }
 
 export default async function PostPage({ params }: PostPageProps) {
-    const { slug } = await params;
-    const post = await getPostPageProps(slug);
+    const { slug } = params;
+
+    const posts: PostData[] = await getAllPosts('_sort=id&_order=desc&_start=0&_limit=30');
+    const post = posts.find((p) => p.slug === slug);
 
     if (!post) {
-        return notFound();
+        return <div>Post não encontrado</div>;
     }
 
-    return (<Post post={post} />);
+    return <PostComponent post={post} />;
 }
 
-// mapeia o post e gera o post.slug
-export async function generateStaticParams() {
+export async function generateStaticParams(): Promise<{ slug: string }[]> {
     const numberOfPosts = await countAllPosts();
-    const posts = await getAllPosts(`_limit=${numberOfPosts}`);
+    const posts: PostData[] = await getAllPosts(`_limit=${numberOfPosts}`);
 
-    return posts.map((post: any) => ({
+    return posts.map((post) => ({
         slug: post.slug,
     }));
 }
-
-// pega o slug[0]
-export const getPostPageProps = async (slug: string) => {
-    const postArray = await getPost(slug);
-    const post = postArray[0];
-    return post;
-};
