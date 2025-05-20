77a1b2e657065c4994f58cf65ebfb7374f91b5a0
fenix <fenix.azul09@protonmail.com>
2025-05-20 19:15:16 -0300

    "Concertando problema do tipo do params"

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -2,15 +2,14 @@ import { getAllPosts } from "@/data/posts/get-all-posts";
 import { countAllPosts } from "@/data/posts/count-all-posts";
 import { Post as PostComponent } from "@/containers/Post";
 import { PostData } from "@/domain/posts/post";
+import { JSX } from "react";
 
 interface PostPageProps {
-    params: {
-        slug: string;
-    };
+    params: Promise<{ slug: string }>; // type de saida para o build
 }
 
-export default async function PostPage({ params }: PostPageProps) {
-    const { slug } = params;
+export default async function PostPage({ params }: PostPageProps): Promise<JSX.Element> {
+    const { slug } = await params;
 
     const posts: PostData[] = await getAllPosts('_sort=id&_order=desc&_start=0&_limit=30');
     const post = posts.find((p) => p.slug === slug);
--- src/utils/fetch-json.ts
+++ src/utils/fetch-json.ts
@@ -5,8 +5,6 @@ export type HomePageProps = {
 }
 
 export const fetchJson = async <T>(url: string): Promise<HomePageProps> => {
-    console.log("Chamando API em:", url);
-
     const rawData = await fetch(url, { next: { revalidate: 60 } });
     rawData.headers.set("Access-Control-Allow-Origin", "*");
 
