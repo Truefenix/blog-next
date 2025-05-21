705a5e69ab3caeb61ef984fe85959613c608a82b
fenix <fenix.azul09@protonmail.com>
2025-05-20 23:33:44 -0300

    "Adicionado deploy:static script"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { getAllPosts } from "../data/posts/get-all-posts";
 
 export default async function Home() {
     const posts = await getAllPosts('_sort=id&_order=desc&_start=0&_limit=30');
-    console.log(posts);
 
     return <HomePage posts={posts} />;
 }
